f46238416fc91e5ee581d5426fe0198bf768141e
Sam Nosenzo <samnosenzo@gmail.com>
2017-11-16 18:51:04 -0500

    Initial Commit

---
+++ LightingProject3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LightingProject3
+//
+//  Created by Sam Nosenzo on 11/16/17.
+//  Copyright © 2017 Sam Nosenzo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
